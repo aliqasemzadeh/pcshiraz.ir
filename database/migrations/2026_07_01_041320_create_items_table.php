97ab908069d6724b418a12624ed3aa521e0d2db8
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2026-07-01 08:25:25 +0330

    add `weight`, `length`, and `width` fields to items table migration

--- database/migrations/2026_07_01_041320_create_items_table.php
+++ database/migrations/2026_07_01_041320_create_items_table.php
@@ -21,6 +21,7 @@ return new class extends Migration
             $table->string('title');
             $table->text('description')->nullable();
             $table->string('color_code')->nullable();
+
             $table->integer('weight')->nullable();
             $table->integer('length')->nullable();
             $table->integer('width')->nullable();
