5b22ebe02b80ebf8e074052bc1ee41e609a90b5b
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2026-07-01 12:00:15 +0330

    add one-time passwords migration, integrate Spatie OTP package, and update User model for OTP support

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,13 +10,15 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Spatie\OneTimePasswords\Models\Concerns\HasOneTimePasswords;
+use Spatie\Permission\Traits\HasRoles;
 
 #[Fillable(['mobile', 'first_name', 'last_name', 'email', 'password', 'national_code', 'birth_date'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, HasOneTimePasswords, HasRoles, Notifiable;
 
     /**
      * Get the attributes that should be cast.
