f7f6b9cec7231d3bd1e63832049d40c5735f0056
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2026-08-01 11:45:04 +0330

    feat: implement Livewire modals with Alpine.js directives, modal stacking, and dynamic positioning for enhanced UI flexibility

---
+++ resources/views/vendor/livewire-modal/components/modal.blade.php
@@ -0,0 +1,25 @@
+@props([
+    'position' => 'center',
+    'xData' => '',
+])
+
+<div x-data="{
+    position: @js($position),
+    {!! str($xData)->ltrim('{')->rtrim('}') !!}
+}"
+    x-bind:class="{
+        'm-auto': position === 'center',
+        'mr-auto my-auto': position === 'left',
+        'ml-auto my-auto': position === 'right',
+        'me-auto my-auto': position === 'start',
+        'ms-auto my-auto': position === 'end',
+        'mb-auto mx-auto': position === 'top',
+        'mt-auto mx-auto': position === 'bottom',
+        'mb-auto mr-auto': position === 'top-left',
+        'mb-auto ml-auto': position === 'top-right',
+        'mt-auto mr-auto': position === 'bottom-left',
+        'mt-auto ml-auto': position === 'bottom-right',
+    }"
+    x-bind="modalAttributes" {!! $attributes->class(['max-w-full min-w-0 transition']) !!}>
+    {{ $slot }}
+</div>
