a24f115c9bd52b48fd8dbe7840433c8d263ecc56
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2026-08-17 13:06:56 +0330

    feat: introduce `UpdatePermissionCommand` skeleton
    - Added a new console command `UpdatePermissionCommand` with attributes for signature and description.
    - Prepared the structure for future permission update functionality.

---
+++ app/Console/Commands/UpdatePermissionCommand.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Attributes\Description;
+use Illuminate\Console\Attributes\Signature;
+use Illuminate\Console\Command;
+
+#[Signature('app:update-permission-command')]
+#[Description('Command description')]
+class UpdatePermissionCommand extends Command
+{
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+    }
+}
