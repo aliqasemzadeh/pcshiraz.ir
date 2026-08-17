91f15c23ec18f1f34dcb1b5f6337d066237f88c8
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2026-08-17 10:14:09 +0330

    feat: add `UpdatePriceCommand` and `UpdatePriceJob` console commands
    - Introduced skeleton classes for `UpdatePriceCommand` and `UpdatePriceJob` with attributes for signature and description.
    - Prepared structure for future price update functionality.

---
+++ app/Console/Commands/UpdatePriceJob.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Attributes\Description;
+use Illuminate\Console\Attributes\Signature;
+use Illuminate\Console\Command;
+
+#[Signature('app:update-price-job')]
+#[Description('Command description')]
+class UpdatePriceJob extends Command
+{
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+    }
+}
